be8d3ddfb130768ff7964247165f5d237c8ec390
zhangyue <zhangy1@pxing.com>
2023-11-02 13:31:31 +0800

    fix  bug

--- test_case/test_2_homepage/test_1_ride/test_3_banner.py
+++ test_case/test_2_homepage/test_1_ride/test_3_banner.py
@@ -32,7 +32,6 @@ class TestHomepageRideBanner:
             time.sleep(3)
             # 验证地图已加载
             assert assert_element_exist(device(label="用车规则"))
-            attach_screenshot(device, '点击banner-找车后截图')
         except AssertionError as e:
             logger.error("验证骑行页面banner-找车功能失败")
             raise e
@@ -40,6 +39,7 @@ class TestHomepageRideBanner:
             logger.error("验证骑行页面banner-找车功能失败，未知错误：{}".format(e))
             raise AssertionError("验证骑行页面banner-找车功能失败，未知错误：{}".format(e))
         finally:
+            attach_screenshot(device, '点击banner-找车后截图')
             back_to_ride(device, info.bundle_id)
             time.sleep(1)
     
@@ -53,7 +53,6 @@ class TestHomepageRideBanner:
             device(label="客服中心").click(timeout=3)
             # 通过【计费规则】验证是否进入客服中心
             assert assert_element_exist(device(label="猜你想问"))
-            attach_screenshot(device, '点击客服中心后截图')
             time.sleep(1)  # 等待页面加载
         except AssertionError as e:
             logger.error("验证骑行页面banner-客服中心失败，断言失败")
@@ -62,6 +61,7 @@ class TestHomepageRideBanner:
             logger.error("验证骑行页面banner-客服中心失败，未知错误：{}".format(e))
             raise AssertionError("验证骑行页面banner-客服中心失败，未知错误：{}".format(e))
         finally:
+            attach_screenshot(device, '点击客服中心后截图')
             back_to_ride(device, info.bundle_id)
             time.sleep(1)
     
@@ -74,7 +74,6 @@ class TestHomepageRideBanner:
             device(label="故障上报").click(timeout=3)
             # 通过【上传车辆编号】文字验证是否进入故障上报页面
             assert assert_element_exist(device(label="上传车辆编号"))
-            attach_screenshot(device, '点击故障上报后截图')
         except AssertionError as e:
             logger.error("验证骑行页面banner-故障上报失败")
             raise e
@@ -82,6 +81,7 @@ class TestHomepageRideBanner:
             logger.error("验证骑行页面banner-故障上报失败，未知错误：{}".format(e))
             raise AssertionError("验证骑行页面banner-故障上报失败，未知错误：{}".format(e))
         finally:
+            attach_screenshot(device, '点击故障上报后截图')
             # 点击返回按钮
             back_to_ride(device, info.bundle_id)
             time.sleep(1)
@@ -97,7 +97,6 @@ class TestHomepageRideBanner:
             # 通过【用车】【停车】按钮文字验证是否进入申请停车点页面
             assert assert_element_exist(device(label="用车"))
             assert assert_element_exist(device(label="停车"))
-            attach_screenshot(device, '点击申请停车点后截图')
         except AssertionError as e:
             logger.error("验证骑行页面banner-申请停车点失败")
             raise e
@@ -105,6 +104,7 @@ class TestHomepageRideBanner:
             logger.error("验证骑行页面banner-申请停车点失败，未知错误：{}".format(e))
             raise AssertionError("验证骑行页面banner-申请停车点失败，未知错误：{}".format(e))
         finally:
+            attach_screenshot(device, '点击申请停车点后截图')
             # 点击返回按钮
             back_to_ride(device, info.bundle_id)
             time.sleep(1)
@@ -118,13 +118,12 @@ class TestHomepageRideBanner:
             for i in range(5):
                 # 滑动banner至违规举报
                 device.swipe(0.8, 0.4, 0.2, 0.4)
-                if assert_element_exist(device(label="违规举报"), retry_times=1):
+                if assert_element_exist(device(label="违规举报"), retry_times=3):
                     break
                 time.sleep(1)
             device(label="违规举报").click(timeout=3)
             # 通过【上传违规车辆编号】文字验证是否进入违规举报页面
             assert assert_element_exist(device(label="上传违规车辆编号"))
-            attach_screenshot(device, '点击违规举报后截图')
         except AssertionError as e:
             logger.error("验证骑行页面banner-违规举报失败")
             raise e
@@ -133,6 +132,7 @@ class TestHomepageRideBanner:
             raise AssertionError("验证骑行页面banner-违规举报失败，未知错误：{}".format(e))
         finally:
             # 点击返回按钮
+            attach_screenshot(device, '点击违规举报后截图')
             back_to_ride(device, info.bundle_id)
             time.sleep(1)
     
@@ -153,13 +153,13 @@ class TestHomepageRideBanner:
                 device.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/StaticText[1]'))
             assert device.xpath(
                 '//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/StaticText[1]').get().value == '优惠购卡专区', '进入今日福利页面失败'
-            attach_screenshot(device, '点击今日福利后截图')
         except AssertionError as e:
             logger.error("验证骑行页面banner-今日福利失败")
         except Exception as e:
             logger.error("验证骑行页面banner-今日福利失败，未知错误：{}".format(e))
             raise AssertionError("验证骑行页面banner-今日福利失败，未知错误：{}".format(e))
         finally:
+            attach_screenshot(device, '点击今日福利后截图')
             # 点击返回按钮
             back_to_ride(device, info.bundle_id)
             back_to_homepage(device, info.bundle_id)
